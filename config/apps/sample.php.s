6b61110f7a046c64fc1ec2138079b161f7dcab73
Rocky <wpkpda@gmail.com>
2019-10-10 18:55:46 +0600

    added sample

---
+++ config/apps/sample.php.s
@@ -0,0 +1,10 @@
+<?php
+
+return         [
+    'id' => 12345,
+    'name' => 'Equity Release',
+    'key' => 'weqfwqefqfqvqvvfq',
+    'secret' => 'quefcycfquywfhqwegfqwfvqgasrgasdgas',
+    'enable_client_messages' => false,
+    'enable_statistics' => true,
+];
